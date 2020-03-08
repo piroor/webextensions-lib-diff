41c10323687707123e4d745b7b4560020e16644b
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-08 20:22:26 +0900

    Make test more robust

--- test/test-dom-updater.js
+++ test/test-dom-updater.js
@@ -12,78 +12,89 @@ const dom = new JSDOM();
 const document = dom.window.document;
 
 function createNode(source) {
-  const range = document.createRange();
-  range.setStart(document.body, 0);
   const node = document.createElement('div');
-  node.appendChild(range.createContextualFragment(source.trim()));
-  range.detach();
+  node.appendChild(createFragment(source));
   return node;
 }
 
+function createFragment(source) {
+  const range = document.createRange();
+  range.setStart(document.body, 0);
+  const contents = range.createContextualFragment(source.trim());
+  range.detach();
+  return contents;
+}
+
 function assertUpdated(from, to, steps) {
-  const expected = createNode(to.innerHTML);
-  const actualSteps = DOMUpdater.update(from, to);
-  is(expected.innerHTML, from.innerHTML);
+  const fromNode = createNode(from);
+  const actualSteps = DOMUpdater.update(fromNode, createFragment(to));
+  is(createNode(to).innerHTML, fromNode.innerHTML);
   if (typeof steps == 'number')
     is(steps, actualSteps);
 }
 
 export function testUpdateAttributes() {
   assertUpdated(
-    createNode(`
-      <span class="class1 class2"
-            data-updated="true"
-            data-removed="true">contents</span>
-    `),
-    createNode(`
-      <span class="class1 class2 class3"
-            data-updated="false"
-            data-added="true">contents</span>
-    `),
+    `<span class="class1 class2"
+           data-updated="true"
+           data-removed="true">contents</span>`,
+    `<span class="class1 class2 class3"
+           data-updated="false"
+           data-added="true">contents</span>`,
     4
   );
 }
 
 export function testUpdateNodes() {
   assertUpdated(
-    createNode(`
-      <span id="item1">contents</span>
-      <span id="item2">contents</span>
-      <span id="item3">contents</span>
-      <span id="item4">contents</span>
-      <span id="item5">contents</span>
-      <span id="item6">contents</span>
-    `),
-    createNode(`
-      <span id="item3">contents</span>
-      <span id="item4">contents</span>
-      <span id="item5">contents</span>
-      <span id="item6">contents</span>
-      <span id="item7">contents</span>
-      <span id="item8">contents</span>
-    `),
+    `<span id="item1">contents</span>
+     <span id="item2">contents</span>
+     <span id="item3">contents</span>
+     <span id="item4">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6">contents</span>`,
+    `<span id="item3">contents</span>
+     <span id="item4">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6">contents</span>
+     <span id="item7">contents</span>
+     <span id="item8">contents</span>`,
     4 /* deletion */ + 4 /* insertion */
+  );
+}
+
+export function testUpdateReplacedNodes() {
+  assertUpdated(
+    `<span id="item1">contents</span>
+     <span id="item2">contents</span>
+     <span id="item3">contents</span>
+     <span id="item4">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6">contents</span>`,
+    `<span id="item1">contents</span>
+     <span id="item2">contents</span>
+     <span id="item7">contents</span>
+     <span id="item8">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6">contents</span>`,
+    2 /* deletion */ + 2 /* insertion */
   );
 }
 
 export function testUpdateNodesAndAttributes() {
   assertUpdated(
-    createNode(`
-      <span id="item1">contents</span>
-      <span id="item2">contents</span>
-      <span id="item3" part="active">contents, active</span>
-      <span id="item4">contents</span>
-      <span id="item5">contents</span>
-      <span id="item6">contents</span>
-    `),
-    createNode(`
-      <span id="item3">contents, old active</span>
-      <span id="item4">contents</span>
-      <span id="item5">contents</span>
-      <span id="item6" part="active">contents, new active</span>
-      <span id="item7">contents</span>
-      <span id="item8">contents</span>
-    `),
+    `<span id="item1">contents</span>
+     <span id="item2">contents</span>
+     <span id="item3" part="active">contents, active</span>
+     <span id="item4">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6">contents</span>`,
+    `<span id="item3">contents, old active</span>
+     <span id="item4">contents</span>
+     <span id="item5">contents</span>
+     <span id="item6" part="active">contents, new active</span>
+     <span id="item7">contents</span>
+     <span id="item8">contents</span>`,
     4 /* item deletion */ + 4 /* iteminsertion */ +
       1 /* remove attr */ + 1 /* replace text */ +
       1 /* remove attr */ + 1 /* replace text */
@@ -92,21 +103,17 @@ export function testUpdateNodesAndAttributes() {
 
 export function testUpdateWithNoHint() {
   assertUpdated(
-    createNode(`
-      <span>contents 1</span>
-      <span>contents 2</span>
-      <span part="active">contents 3, active</span>
-      <span>contents 4</span>
-      <span>contents 5</span>
-      <span>contents 6</span>
-    `),
-    createNode(`
-      <span>contents 3, old active</span>
-      <span>contents 4</span>
-      <span>contents 5</span>
-      <span part="active">contents 6, new active</span>
-      <span>contents 7</span>
-      <span>contents 8</span>
-    `)
+    `<span>contents 1</span>
+     <span>contents 2</span>
+     <span part="active">contents 3, active</span>
+     <span>contents 4</span>
+     <span>contents 5</span>
+     <span>contents 6</span>`,
+    `<span>contents 3, old active</span>
+     <span>contents 4</span>
+     <span>contents 5</span>
+     <span part="active">contents 6, new active</span>
+     <span>contents 7</span>
+     <span>contents 8</span>`
   );
 }
